7fa9181ffbcf093c62ab23b07349ff3c2b5462f7
Elias Santos Jr <elias.jr.dev@gmail.com>
2024-01-22 23:39:07 -0300

    Step 7: JSON Model

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,12 +1,24 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
-], function(Controller, MessageToast) {
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel"
+], function(Controller, MessageToast, JSONModel) {
     'use strict';
     
     return Controller.extend("ui5.walkthrough.controller.App", {
+        onInit(){
+            //set data model on View
+            const oData={
+                recipient:{
+                    name: "Camarada"
+                }
+            };
+            const oModel = new JSONModel(oData);
+            this.getView().setModel(oModel);
+        },
+
         onShowHello(){
-            MessageToast.show("Auf Wierdersen");
+            MessageToast.show("Auf Wierdersen Asshole");
         }
     });
 });
